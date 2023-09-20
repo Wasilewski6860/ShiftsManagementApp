9f601a88beb86007b81181d27314b2943c2c459d
Wasilewski6860 <trollivanovich6860@gmail.com>
2023-09-21 00:58:16 +0300

    Architecture changes

--- app/src/main/java/com/example/newtryofappwithworkingshifts/data/WorkShift.kt
+++
@@ -1,6 +0,0 @@
-package com.example.newtryofappwithworkingshifts.data
-
-data class WorkShift(
-    var name : String  = "Test",
-    var color : Int =0
-):java.io.Serializable // serializing the model
